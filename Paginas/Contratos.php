3f8e65af9ab56a03892e35c6db4a57f9b8a03e08
narenvoos <voosnaren40@gmail.com>
2024-07-12 17:21:41 -0500

    Actualizacion con la pagina contratos

--- Paginas/Contratos.php
+++ Paginas/Contratos.php
@@ -12,13 +12,12 @@ if(!isset($_SESSION["profile"])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Empleado</title>
     <script src="../assets/sweetalert2/sweetalert2.all.min.js"></script>
-   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+   <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">-->
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"/>
 
 </head>
-
 
 <body>
     <?php require_once('../componentes/navbar.php')?>
@@ -28,20 +27,27 @@ if(!isset($_SESSION["profile"])){
                 <form id="frmCrearEmpleado" class="mt-4">
                     <div class="container-fluid d-flex align-items-center">
 
-                        <h5>DATOS DEL EMPLEADO</h5>
+                        <h5>DATOS DEL CONTRATO</h5>
                     </div>
 
                     <div class="row" id="">
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label for="campo3">NOMBRE COMPLETO</label>
+                                <label for="campo3">NUMERO DE DOCUMENTO</label>
                                 <!--Nombre completo--->
                                 <input type="text" class="form-control" id="nombreEmpleado" name="nombreEmpleado">
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label for="campo5">FECHA DE NACIMIENTO</label>
+                                <label for="campo3">NOMBRE DEL EMPLEADO</label>
+                                <!--Nombre completo--->
+                                <input type="text" class="form-control" id="nombreEmpleado" name="nombreEmpleado">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="campo5">EMPRESA</label>
                                 <!--Numero de identificacion --->
                                 <input type="date" class="form-control" id="numeroDocumento" name="numeroDocumento">
                             </div>
@@ -49,118 +55,85 @@ if(!isset($_SESSION["profile"])){
                         <div class="col-md-6">
                             <!--tipo de documento-->
                             <div class="form-group">
-                                <label for="">TIPO DE DOCUMENTO</label>
+                                <label for="">CARGO</label>
                                 <select class="form-control" id="tipoDocumento" name="tipoDocumento">
                                 </select>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label for="campo5">NUMERO DE DOCUMENTO</label>
+                                <label for="campo5">AUXILIO DE TRANSPORTE (SI APLICA )</label>
                                 <!--Numero de identificacion --->
-                                <input type="text"  class="form-control" id="numeroDocumento" name="numeroDocumento">
+                                <input type="checkbox" class="form-control" id="numeroDocumento" value="SI"name="numeroDocumento">SI
+                                <input type="checkbox" class="form-control" id="numeroDocumento" value="NO"name="numeroDocumento">NO
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label for="campo5">FECHA DE EXPEDICION DEL DOCUMENTO</label>
+                                <label for="campo5">AUXILIOS ADICIONALES (SI APLICA )</label>
+                                <!--Numero de identificacion --->
+                                <input type="checkbox" class="form-control" id="numeroDocumento" value="SI"name="numeroDocumento">SI
+                                <input type="checkbox" class="form-control" id="numeroDocumento" value="NO"name="numeroDocumento">NO
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <p>En caso de aplicar auxilios:</p>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="campo5">TIPO DE AUXILIO</label>
+                                <!--Numero de identificacion --->
+                                <input type="text" class="form-control" id="numeroDocumento" name="numeroDocumento">
+                                <label for="campo5">MONTO DE AUXILIO</label>
+                                <input type="text" class="form-control" id="numeroDocumento" name="numeroDocumento">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="campo5">TIPO DE CONTRATO</label>
                                 <!--Numero de identificacion --->
                                 <input type="date" class="form-control" id="numeroDocumento" name="numeroDocumento">
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label for="campo5">NUMERO DE TELEFONO</label>
+                                <label for="campo5">SALARIO</label>
                                 <!--Numero de identificacion --->
                                 <input type="text" class="form-control" id="numeroDocumento" name="numeroDocumento">
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label for="campo5">CORREO ELECTRONICO</label>
+                                <label for="campo5">FECHA DE INGRESO</label>
                                 <!--Numero de identificacion --->
-                                <input type="email" class="form-control" id="numeroDocumento" name="numeroDocumento">
+                                <input type="date" class="form-control" id="numeroDocumento" name="numeroDocumento">
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label for="campo5">DIRECCION DE DOMICILIO</label>
+                                <label for="campo5">FECHA VENCIMIENTO</label>
                                 <!--Numero de identificacion --->
-                                <input type="text" class="form-control" id="numeroDocumento" name="numeroDocumento">
+                                <input type="date" class="form-control" id="numeroDocumento" name="numeroDocumento">
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label for="campo5">TIPO DE SANGRE RH</label>
+                                <label for="campo5">PRORROGA</label>
                                 <!--Numero de identificacion --->
-                                <input type="text" class="form-control" id="numeroDocumento" name="numeroDocumento">
+                                <input type="checkbox" class="form-control" id="numeroDocumento" name="numeroDocumento">SI
+                                <input type="checkbox" class="form-control" id="numeroDocumento" name="numeroDocumento">NO
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label for="campo5">EPS </label>
+                                <label for="campo5">NUEVA FECHA </label>
                                 <!--Salario empleado --->
-                                <input type="text" class="form-control" id="salarioEmpleado" name="EPS">
+                                <input type="date" class="form-control" id="salarioEmpleado" name="EPS">
                             </div>
                         </div>
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="campo5">ARL</label>
-                                <!--Salario empleado --->
-                                <input type="text" class="form-control" id="salarioEmpleado" name="PENSION">
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="campo5">FONDO DE PENSIONES </label>
-                                <!--Salario empleado --->
-                                <input type="text" class="form-control" id="salarioEmpleado" name="PENSION">
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="campo5">FONDO DE CESANTIAS </label>
-                                <!--Salario empleado --->
-                                <input type="text" class="form-control" id="salarioEmpleado" name="PENSION">
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="campo5">CAJA DE COMPENSACION </label>
-                                <!--Salario empleado --->
-                                <input type="text" class="form-control" id="salarioEmpleado" name="PENSION">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="container-fluid d-flex align-items-center">
-                        <h5>DATOS DEL FAMILIAR DE CONTACTO </h5>
-                    </div>
-                    <div class="row" id="">
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="campo5">NOMBRE COMPLETO </label>
-                                <!--Salario empleado --->
-                                <input type="text" class="form-control" id="salarioEmpleado" name="PENSION">
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="campo5">NUMERO DE TELEFONO </label>
-                                <!--Salario empleado --->
-                                <input type="text" class="form-control" id="salarioEmpleado" name="PENSION">
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="campo5">PARENTESCO</label>
-                                <!--Salario empleado --->
-                                <input type="text" class="form-control" id="salarioEmpleado" name="PENSION">
-                            </div>
-                        </div>
-                    </div>
-
-
-                    <input type="text" class="form-control" id="idPaciente" name="idPaciente" style="display:none;">
+                       <input type="text" class="form-control" id="idPaciente" name="idPaciente" style="display:none;">
                     <button type="submit" class="btn btn-success mt-3" id="buttonCrearEmpleado"><i class="fa-regular fa fa-save"></i> Guardar
                         empleado</button>
                 </form>
@@ -177,11 +150,7 @@ if(!isset($_SESSION["profile"])){
     <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/solid.js" integrity="sha384-/BxOvRagtVDn9dJ+JGCtcofNXgQO/CCCVKdMfL115s3gOgQxWaX/tSq5V8dRgsbc" crossorigin="anonymous"></script>
     <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/fontawesome.js" integrity="sha384-dPBGbj4Uoy1OOpM4+aRGfAOc0W37JkROT+3uynUgTHZCHZNMHfGXsmmvYTffZjYO" crossorigin="anonymous"></script>
 
-    <script>
-    document.addEventListener('DOMContentLoaded', e => {
-        $('#input-datalist').autocomplete()
-    }, false);
-</script>
+
 
 
 </body>
